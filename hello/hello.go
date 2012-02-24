c75107f7759ff9803d87e800022ba78f982aa8ad
Kicool <kicool@foxmail.com>
2012-02-24 11:45:51 +0800

    Add sample hello

---
+++ hello/hello.go
@@ -0,0 +1,15 @@
+/*
+ * kicool@foxmail.com
+ * 2012.02.24
+ * my first go sample
+ *
+ */
+package main
+
+import (
+	"fmt" // Package implementing formatted I/O.
+)
+
+func main() {
+	fmt.Printf("Hello, world; or Καλημέρα κόσμε; or こんにちは 世界\n")
+}
