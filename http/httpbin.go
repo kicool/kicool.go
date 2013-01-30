cb32fa42d92ef4dc80fc2e0748bebc13e6b469ef
kicool <kicool@gmail.com>
2013-01-30 19:11:41 +0800

    add getUA func in httpbin.go

--- http/httpbin.go
+++ http/httpbin.go
@@ -32,45 +32,67 @@ func help(u *url.URL) {
 	log.Println(string(body))
 }
 
-func getIP(u *url.URL) {
-	log.Println("get origin IP")
+func getIP(u *url.URL) string {
+	ip, err := getJsonResp(u, "/ip")
+	if err == nil {
+		log.Println("Your IP:", ip["origin"])
+		return ip["origin"]
+	}
+	return ""
+}
 
-	u.Path = "/ip"
+func getUA(u *url.URL) string {
+	ua, err := getJsonResp(u, "/user-agent")
+	if err == nil {
+		log.Println("Your UA:", ua["user-agent"])
+		return ua["user-agent"]
+	}
+	return ""
 
-	log.Println("get from ", u)
+}
+
+func getJsonResp(u *url.URL, p string) (map[string]string, error) {
+	u.Path = p
+
+	log.Println("getJsonResp from ", u)
 
 	resp, err := http.Get(u.String())
 	if err != nil {
 		log.Fatalln("http.Get", err)
+		return nil, err
 	}
 	defer resp.Body.Close()
 
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		log.Fatalln("ioutil.ReadAll", err)
+		return nil, err
 	}
 
 	//log.Println(string(body))
 
-	type IPJson struct {
-		Origin string
-	}
-	var ipjson IPJson
-	err = json.Unmarshal(body, &ipjson)
+	jsonBlob := make(map[string]string)
+	err = json.Unmarshal(body, &jsonBlob)
 	if err != nil {
 		log.Println(err)
+		return nil, err
 	}
 
-	log.Println("Your ip is", ipjson.Origin)
+	log.Println("Response", jsonBlob)
+	return jsonBlob, nil
 }
 
 func main() {
 	flag.Parse()
+
+	log.SetFlags(log.Lmicroseconds)
 
 	if *h {
 		help(u)
 	}
 
 	getIP(u)
+
+	getUA(u)
 
 }
